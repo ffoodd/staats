3718dd684a8e38d3e37cdd44367a70eafaf61c9e
ffoodd <gael.poupard@gmail.com>
2016-11-14 22:55:19 +0100

    feat(a11y): first try with a11y

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var a11y = require('a11y');
 
-gulp.task('default', function() {
-
+gulp.task('a11y', function() {
+  return a11y('https://aduggin.github.io/accessibility-fails/', function (err, reports) {
+      reports.audit.forEach(function (el) {
+        console.log(el);
+      });
+  });
 });
