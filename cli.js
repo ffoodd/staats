4160f54905b91d8c408ef39dd9b71e7e538d2481
POUPARD gael <gael.poupard@orange.com>
2016-11-17 14:07:58 +0100

    feat(output URL): simple first function

---
+++ cli.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+const meow    = require('meow')
+const shtml   = require('shtml')
+const isBlank = require('is-blank')
+const staats  = require('./')
+
+const cli = meow(shtml`
+  <div>
+    <underline>Usage</underline>
+    $ staats [options...]<br><br>
+    <underline>Options</underline>
+    -h, --help - Get help menu
+    <underline>Examples</underline>
+    $ staats --help<br><br>
+`, {
+  alias: {
+    h: 'help'
+  }
+})
+
+const url = cli.input[0]
+
+console.log(staats(url));
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-#!/usr/bin/env node
- console.log('Hello, world!');
+'use strict'
+
+const isBlank      = require('is-blank')
+const normalizeUrl = require('normalize-url')
+
+module.exports = (url) => {
+  if (isBlank(url) || typeof url !== 'string') {
+    throw new TypeError('staats expected a url as a string')
+  }
+
+  url = normalizeUrl(url)
+
+  return url;
+}
