452c4729a972d26d39afcafc1c358e0260d62c6a
POUPARD gael <gael.poupard@orange.com>
2016-11-17 14:30:29 +0100

    feat(a11y): added Addy Osmani's a11y

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const isBlank = require('is-blank')
 const nUrl    = require('normalize-url')
 const psi     = require('psi')
+const a11y    = require('a11y')
 
 module.exports = (url) => {
   if (isBlank(url) || typeof url !== 'string') {
@@ -14,5 +15,20 @@ module.exports = (url) => {
   psi(url, {nokey: 'true', strategy: 'mobile'}).then(data => {
     console.log('Speed score: ' + data.ruleGroups.SPEED.score);
     console.log('Usability score: ' + data.ruleGroups.USABILITY.score);
+  });
+
+
+  a11y(url, function (err, reports) {
+    if (err) {
+      console.error(err.message);
+      process.exit(err.code || 1);
+    }
+
+    reports.audit.forEach(function (el) {
+      if (el.result === 'FAIL') {
+        console.log(el.severity + ': ' + el.heading);
+        console.log(el.elements);
+      }
+    });
   });
 }
