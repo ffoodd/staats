59f18c9c68867c87f9f906e8b6f7d114108b5cae
ffoodd <gael.poupard@gmail.com>
2018-04-26 21:22:12 +0200

    feat: more accurate symbol/status for results

--- index.js
+++ index.js
@@ -22,21 +22,38 @@ module.exports = (url) => {
   };
 
   url = nUrl(url);
-  
+
   function displayTitle(title) {
     console.log(`\n
         ${chalk.inverse.bold(title)}
     `)
   };
-  
+
   function displayTest(test) {
-    let symbol = (test.status ? symbols.success : symbols.error);
+    switch(test.status) {
+      case 'parse-error':
+      case 'error':
+      case 'FAIL':
+      case false:
+        var symbol = symbols.error;
+        break;
+      case 'warning':
+        var symbol = symbols.warning;
+        break;
+      case 'info':
+      case 'NA':
+        var symbol = symbols.info;
+        break;
+      case true:
+      default:
+        var symbol = symbols.success;
+    }
 
     console.log(chalk`${symbol} {bold ${test.name}} ${test.value}`)
   };
-  
+
   /* Using Generators?
-  @see https://hacks.mozilla.org/2015/05/es6-in-depth-generators/  
+  @see https://hacks.mozilla.org/2015/05/es6-in-depth-generators/
   function* displayresults(results) {
     for (let result in results) {
       yield result;
@@ -46,43 +63,42 @@ module.exports = (url) => {
   html({url: url})
     .then(data => {
       displayTitle('HTML validation')
-      
+
       let messages = JSON.parse(data);
       let results  = messages.messages.reduce((results, value, key) => { results[key] = value; return results; }, {});
 
       for (let result in results) {
         let test = new Object(results[result]);
-          test.status = (results[result].type == "error");
+          test.status = results[result].type;
           test.name   = results[result].message;
           if (!results[result].firstLine) {
             test.firstLine = results[result].lastLine;
           }
           test.value  = `From line ${results[result].firstLine}, column ${results[result].firstColumn}; to line ${results[result].lastLine}, column ${results[result].lastColumn}`;
-          
         displayTest(test)
       }
     })
     .catch((error) => {console.error(error)});
-  
+
   css.validate(url, function(error, data) {
     displayTitle('CSS validation')
-    
+
     if(error) {
       console.error(error)
     } else {
       let results = data.errors;
-      
+
       for (let result in results) {
         let test = new Object(results[result]);
-          test.status = (results[result].errorType != "parse-error");
+          test.status = results[result].errorType;
           test.name   = results[result].message;
           test.value  = `at line ${results[result].line}`;
-          
+
         displayTest(test)
       }
     }
   });
-    
+
   seo.load(url, function(data) {
     displayTitle('SEO')
 
@@ -90,13 +106,13 @@ module.exports = (url) => {
       console.error('SEO checker failed :/')
     } else {
       let results = seo.meta(data);
-      
+
       for (let result in results) {
         let test = new Object(result);
           test.status = !isBlank(results[result]);
           test.name   = result;
           test.value  = results[result];
-          
+
         displayTest(test)
       }
     }
@@ -111,39 +127,41 @@ module.exports = (url) => {
   stats.parse()
     .then(data => {
       displayTitle('CSS Stats')
-      
+
       let results = JSON.parse(JSON.stringify(data));
-      
+
       for (let result in results) {
         // if result is propertiesCount, we'll have to do better :)
         let test = new Object(result);
-          // 100 is arbitrary value, need a way to define a budget!
+          // 100 is arbitrary value, need a way to define a per result budget!
           test.status = (parseInt(results[result]) >= 100);
           test.name   = result;
           test.value  = results[result];
-          
+
         displayTest(test)
       }
     })
     .catch((error) => console.error(error));
-  
+
   request(url)
     .then(data => {
       displayTitle('DOM Stats')
 
       let results = dom(data);
-      
+
       for (let result in results) {
         // if result is tagCounts, we'll have to do better :)
-        let test = new Object(result);
-          // 100 is arbitrary value, need a way to define a budget!
-          test.status = (parseInt(results[result]) >= 100);
-          test.name   = result;
-          test.value  = results[result];
-          
-        displayTest(test)
+        if ('tagCounts' != result) {
+          // 10 is arbitrary value, need a way to define a per result budget!
+          let test = new Object(result);
+            test.status = (parseInt(results[result]) < 10);
+            test.name   = result;
+            test.value  = results[result];
+
+          displayTest(test)
+        }
       }
-      
+
       /*displayTitle('CSS browser compat')
 
       let $ = cheerio.load(data);
@@ -156,7 +174,7 @@ module.exports = (url) => {
           .on('data', function(result) {
             // Condition shouldn't exist
             // @link https://github.com/anandthakker/doiuse/issues/89
-            if(undefined !== result.featureData.missing 
+            if(undefined !== result.featureData.missing
               && 'Opera Mini (all)' !== result.featureData.missing
               && 'Opera Mini (all), Opera Mobile (12.1)' !== result.featureData.missing
               && 'IE (11), Opera Mini (all), Opera Mobile (12.1)' !== result.featureData.missing
@@ -165,58 +183,57 @@ module.exports = (url) => {
                 test.status = false;
                 test.name   = result.featureData.title;
                 test.value  = `not supported by ${result.featureData.missing}`;
-                
+
                 displayTest(test)
             }
           });
       });*/
     })
     .catch((error) => console.error(error));
-    
+
   psi(url, {nokey: 'true', strategy: 'mobile'})
     .then(data => {
       displayTitle('Page Speed Insights Scores')
-      
+
       let scores = data.ruleGroups;
-      
+
       for (let result in scores) {
         let test = new Object(result);
           // 90 is arbitrary value, need a way to define a budget!
           test.status = (scores[result].score >= 90);
           test.name   = result;
           test.value  = scores[result].score;
-          
+
         displayTest(test)
       }
-      
+
       displayTitle('Page Speed Insights Stats')
-      
+
       let results = data.pageStats;
-      
+
       for (let result in results) {
         let test = new Object(result);
-          // 100 is arbitrary value, need a way to define a budget!
+          // 100 is arbitrary value, need a way to define a per result budget!
           test.status = (parseInt(results[result]) >= 100);
           test.name   = result;
           test.value  = results[result];
-          
+
         displayTest(test)
       }
-      
+
       displayTitle('Page Speed Insights Tests')
-      
+
       let formattedResults = data.formattedResults.ruleResults;
-      
+
       for (let result in formattedResults) {
         let test = new Object(result);
-          // 100 is arbitrary value, need a way to define a budget!
           test.status = (formattedResults[result].ruleImpact > 0);
           test.name   = formattedResults[result].localizedRuleName;
           test.value  = formattedResults[result].groups;
           if (formattedResults[result].summary) {
             test.value  = formattedResults[result].summary.format;
           }
-          
+
         displayTest(test)
       }
     })
@@ -228,15 +245,15 @@ module.exports = (url) => {
     if (error) {
       console.error(error.message)
     }
-    
+
     let results = data.audit.reduce((results, value, key) => { results[key] = value; return results; }, {});
 
     for (let result in results) {
       let test = new Object(results[result]);
-        test.status = (results[result].result == "FAIL");
+        test.status = results[result].result;
         test.name   = results[result].heading;
         test.value  = results[result].elements.replace(/(\r\n\t|\n|\r\t)/gm, ', ');
-        
+
       displayTest(test)
     }
   });
